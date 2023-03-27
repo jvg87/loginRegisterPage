9b9e0521e9eaf667f8070ff2f36acf4b4b2fdcc0
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-03-27 16:08:48 -0300

    ativando modal login

--- index.js
+++ index.js
@@ -2,3 +2,25 @@ import { MobileNavbar } from "./navBar/MobileNavbar.js";
 
 const mobileNavbar = new MobileNavbar('.mobileMenu', '.navList', '.navList a')
 mobileNavbar.init()
+
+
+const button = document.querySelector('.btnLogin')
+const main = document.querySelector('main')
+
+function toggleLogin(){
+    button.addEventListener('click', () => {
+        main.classList.add('open')
+    })
+}
+
+toggleLogin()
+
+const iconClose = document.querySelector('.iconClose')
+
+function closeLogin(){
+    iconClose.addEventListener('click', () => {
+        main.classList.remove('open')
+    })
+}
+
+closeLogin()
